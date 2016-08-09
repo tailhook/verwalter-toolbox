da035e7876a5737d0ace9819d3556af4edb3c2ca
Paul Colomiets <paul@colomiets.name>
2016-08-09 19:21:10 +0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+=================
+Verwalter Toolbox
+=================
+
+:Status: Alpha
+
+The toolbox is a set of well-tested modules to help you to write your
+verwalter_'s scheduler. In particular:
+
+* Set of utility modules like version comparison
+* A number for helpers to generate test data for your own tests
+
+
+.. _verwalter: http://verwalter.readthedocs.io
---
+++ tests/version.lua
@@ -0,0 +1,41 @@
+local compare = require("modules/version").compare
+local assert = require('assert')
+local busted = require('busted')
+local test = busted.test
+
+test("various comparisons", function()
+    assert.truthy(compare("v1", "v2"))
+    assert.truthy(not compare("v1.0", "v1"))
+    assert.truthy(not compare("v2", "v1"))
+    assert.truthy(compare("v2", "v10"))
+    assert.truthy(compare("v2", "v11"))
+    assert.truthy(not compare("v10", "v2"))
+    assert.truthy(not compare("v12", "v2"))
+    assert.truthy(compare("v1.1", "v2.1"))
+    assert.truthy(compare("v2.1", "v2.3"))
+    assert.truthy(not compare("v2.3", "v2.1"))
+    assert.truthy(compare("v2", "v2.1"))
+end)
+
+-- false for equal versions
+test("compare is false for equal versions", function()
+    assert.falsy(compare("v1", "v1"))
+    assert.falsy(compare("v2", "v2"))
+    assert.falsy(compare("v2.3.4", "v2.3.4"))
+end)
+
+test("version sort", function()
+    local _sorttable = {"v1.1.0", "v1.0", "v2.15", "v3.4.6", "v1", "v2.3",
+                        "v1.1", "v10.0"}
+    table.sort(_sorttable, compare)
+    assert.are.same(_sorttable, {
+        "v1",
+        "v1.0",
+        "v1.1",
+        "v1.1.0",
+        "v2.3",
+        "v2.15",
+        "v3.4.6",
+        "v10.0",
+    })
+end)
