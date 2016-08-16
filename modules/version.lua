7a12f87d3db6543d4a6b0d915f87b91d38246ab9
Paul Colomiets <paul@colomiets.name>
2016-08-16 13:58:18 +0300

    Implement comparison of version against nil

--- modules/version.lua
+++ modules/version.lua
@@ -1,6 +1,12 @@
 local string = require "string"
 
 local function compare(a, b)
+    if a == nil then
+        return not (b == nil)
+    end
+    if b == nil then
+        return false
+    end
     if a:sub(1, 1) == 'v' then a = a:sub(2) end
     if b:sub(1, 1) == 'v' then b = b:sub(2) end
     local aiter = string.gmatch(a, "%w+")
--- tests/version.lua
+++ tests/version.lua
@@ -18,6 +18,12 @@ test("various comparisons", function()
     assert.truthy(compare("v2", "v2.1"))
 end)
 
+test("nil comparison", function()
+    assert.falsy(compare(nil, nil))
+    assert.truthy(compare(nil, "v1"))
+    assert.falsy(compare("v1", nil))
+end)
+
 -- false for equal versions
 test("compare is false for equal versions", function()
     assert.falsy(compare("v1", "v1"))
