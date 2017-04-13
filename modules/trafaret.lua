da5b171c2cfb92a08042b2b7a1f22826ef0f5e7c
Paul Colomiets <paul@colomiets.name>
2017-04-13 15:37:15 +0300

    trafaret: Adds minimal dict support

--- modules/trafaret.lua
+++ modules/trafaret.lua
@@ -1,6 +1,8 @@
 local _Validator = {}
 local _Number = {}
 local _String = {}
+local _Dict = {}
+local _Key = {}
 
 
 function _Number:convert(value, validator, path)
@@ -30,6 +32,84 @@ end
 local function String(_)
     local obj = {}
     setmetatable(obj, {__index=_String})
+    return obj
+end
+
+local function Key(props)
+    local key = props[1]
+    local obj = {
+        key=key,
+        optional=not not props.optional,
+        default=props.default,
+    }
+    setmetatable(obj, {__index=_Key})
+    return obj
+end
+
+function _Key:with_trafaret(trafaret)
+    local obj = {
+        key=self.key,
+        optional=self.optional,
+        default=self.default,
+        trafaret=trafaret,
+    }
+    setmetatable(obj, {__index=_Key})
+    return obj
+end
+
+function _Dict:convert(value, validator, path)
+    if type(value) ~= 'table' then
+        validator:add_error(path, self,
+            "Value is not a table, but", type(value))
+        return {}
+    end
+    local result = {}
+    for key, item in pairs(value) do
+        local keypath = path..'.'..key
+        local keyobj = self.all_keys[key]
+        if not keyobj and not self.allow_extra then
+            validator:add_error(keypath, self, "Unexpected key")
+        else
+            result[key] = keyobj.trafaret:convert(item, validator, keypath)
+        end
+    end
+    for _, key in pairs(self.required_keys) do
+        if result[key] == nil then
+            local default = self.all_keys[key].default;
+            if default ~= nil then
+                result[key] = default
+            else
+                local keypath = path..'.'..key
+                validator:add_error(keypath, self, "Required key")
+            end
+        end
+    end
+    return result
+end
+
+local function Dict(keys)
+    local allow_extra = false
+    local all_keys = {}
+    local required_keys = {}
+    for key, value in pairs(keys) do
+        if key == "allow_extra" then
+            allow_extra = not not value
+        else
+            if type(key) == 'string' then
+                key = Key { key }.with_trafaret(value)
+            else
+                key = key:with_trafaret(value)
+            end
+            all_keys[key.key] = key
+            table.insert(required_keys, key.key)
+        end
+    end
+    local obj = {
+        all_keys=all_keys,
+        required_keys=required_keys,
+        allow_extra=allow_extra,
+    }
+    setmetatable(obj, {__index=_Dict})
     return obj
 end
 
@@ -68,5 +148,7 @@ end
 return {
     Number=Number,
     String=String,
+    Dict=Dict,
+    Key=Key,
     validate=validate,
 }
--- tests/trafaret.lua
+++ tests/trafaret.lua
@@ -43,3 +43,57 @@ describe("trafaret: string", function()
         assert.is.same(err, {": Value is not a string, but table"})
     end)
 end)
+
+describe("trafaret: string", function()
+    local str = T.String{}
+    test("validate str", function()
+        assert(T.validate(str, "xxx"))
+        assert(T.validate(str, "111"))
+    end)
+    test("validate number", function()
+        local res, val, err = T.validate(str, 123)
+        assert(not res)
+        assert.is.same(val, "123")
+        assert.is.same(err, {": Value is not a string, but number"})
+    end)
+    test("validate table", function()
+        local res, _, err = T.validate(str, {})
+        assert(not res)
+        assert.is.same(err, {": Value is not a string, but table"})
+    end)
+end)
+
+describe("trafaret: dict", function()
+    local fixed = T.Dict {
+        [T.Key{"xxx"}] = T.Number{},
+        [T.Key{"yyy"}] = T.Number{},
+    }
+    --[[
+    local extra = T.Dict {
+        [T.Key{"xxx"}] = T.Number{},
+        [T.Key{"yyy"}] = T.Number{},
+        allow_extra=true,
+    }
+    local opt = T.Dict {
+        [T.Key{"xxx", optional=true}] = T.Number{},
+        [T.Key{"yyy"}] = T.Number{},
+    }
+    ]]--
+    test("fixed ok", function()
+        local res, val, _ = T.validate(fixed, {xxx=1, yyy=2})
+        assert(res)
+        assert.is.same(val, {yyy=2, xxx=1})
+    end)
+    test("fixed missing", function()
+        local res, _, _ = T.validate(fixed, {xxx=1})
+        assert(not res)
+    end)
+    test("fixed extra", function()
+        local res, _, _ = T.validate(fixed, {xxx=1, yyy=3, zzz=4})
+        assert(not res)
+    end)
+    test("fixed wrong type", function()
+        local res, _, _ = T.validate(fixed, {xxx="1", yyy=2})
+        assert(not res)
+    end)
+end)
