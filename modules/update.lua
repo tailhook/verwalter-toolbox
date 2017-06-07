560c268fa34de2fcc84808272f61d57856cceaa8
Paul Colomiets <paul@colomiets.name>
2017-06-07 18:23:50 +0300

    Initial pipeline building

--- modules/update.lua
+++ modules/update.lua
@@ -12,16 +12,73 @@ local CONFIG = T.Map { T.String {}, T.Or {
             T.Atom { "smooth_alternate_port" },
             T.Atom { "smooth_same_port" },
             T.Atom { "temporary_shutdown" },
+            T.Atom { "quick_restart_before_test" },
+            T.Atom { "quick_restart" },
+            T.Atom { "quick_restart_after" },
         },
         [T.Key { "test_mode_percent", default=0 }]=T.Number {},
         [T.Key { "warmup_sec", default=1 }]=T.Number {},
     },
 }}
 
+local PIPELINE = T.List { T.Dict {
+    name=T.String {},
+    [T.Key { "forward_mode" }]=
+        T.Or { T.Atom { "manual" }, T.Atom { "time" }, T.Atom { "smooth" } },
+    [T.Key { "forward_time", default=5 }]=T.Number {},
+    [T.Key { "backward_mode" }]=
+        T.Or { T.Atom { "manual" }, T.Atom { "time" }, T.Atom { "smooth" } },
+    [T.Key { "backward_time", default=5 }]=T.Number {},
+    [T.Key { "processes", default={} }]=T.List { T.String {} },
+}}
+
 local function validate_config(config)
     return T.validate(CONFIG, config)
 end
 
+local function derive_pipeline(config)
+    local pipeline = {}
+    for daemon, cfg in pairs(config) do
+        if cfg.kind == "quick_restart" then
+            local insert_idx = 1
+            local inserted = false
+            for idx, s in pairs(pipeline) do
+                if s.name == 'quick_restart' then
+                    table.insert(s.processes, daemon)
+                    if s.forward_time < cfg.warmup_sec then
+                        s.forward_time = cfg.warmup_sec
+                    end
+                    if s.backward_time < cfg.warmup_sec then
+                        s.backward_time = cfg.warmup_sec
+                    end
+                    inserted = true
+                    break
+                elseif s.name == 'test_mode' then
+                    insert_idx = idx+1
+                end
+            end
+            if not inserted then
+                table.insert(pipeline, insert_idx, {
+                    name="quick_restart",
+                    forward_mode="time",
+                    forward_time=cfg.warmup_sec,
+                    backward_mode="time",
+                    backward_time=cfg.warmup_sec,
+                    processes={daemon},
+                })
+            end
+        end
+    end
+    local ok, result, err = T.validate(PIPELINE, pipeline)
+    if not ok then
+        for _, e in pairs(err) do
+            print("Pipeline validation error", e)
+        end
+    end
+    return result
+end
+
 return {
     validate_config=validate_config,
+    derive_pipeline=derive_pipeline,
 }
--- tests/update.lua
+++ tests/update.lua
@@ -3,29 +3,30 @@ local busted = require('busted')
 local test = busted.test
 local describe = busted.describe
 
-local migrations = require("modules/update")
+local update = require("modules/update")
 
-describe("migrations: check process tree", function()
-    test("typical processes", function()
-        local processes = {
-            db_migrate={
-                run_before="test_mode",
-            },
-            celery={
-                kind="temporary_shutdown",
-            },
-            slow_site={
-                kind="smooth_alternate_port",
-                test_mode_percent=5,
-                warmup_sec=15,
-            },
-            fast_site={
-                kind="smooth_same_port",
-                test_mode_percent=5,
-                warmup_sec=1,
-            },
-        }
-        local res, _, err = migrations.validate_config(processes)
+describe("updates: typical setup", function()
+    local processes = {
+        db_migrate={
+            run_before="test_mode",
+        },
+        celery={
+            kind="quick_restart",
+            warmup_sec=5,
+        },
+        slow_site={
+            kind="smooth_alternate_port",
+            test_mode_percent=5,
+            warmup_sec=15,
+        },
+        fast_site={
+            kind="smooth_same_port",
+            test_mode_percent=5,
+            warmup_sec=1,
+        },
+    }
+    test("validate processes", function()
+        local res, _, err = update.validate_config(processes)
         if not res then
             for _, e in pairs(err) do
                 print("Validation error:", e)
@@ -33,5 +34,43 @@ describe("migrations: check process tree", function()
         end
         assert(res)
         --  assert.is.same(processes, converted)  -- defaults are added
+    end)
+end)
+describe("updates: stages", function()
+    test("no processes", function()
+        local stages = update.derive_pipeline({})
+        assert.is.same(stages, {})
+    end)
+    test("quick restart", function()
+        local ok, cfg, _ = update.validate_config({
+            x={kind="quick_restart", warmup_sec=5}
+        })
+        assert(ok)
+        local stages = update.derive_pipeline(cfg)
+        assert.is.same(stages, {{
+            name="quick_restart",
+            processes={"x"},
+            forward_mode="time",
+            forward_time=5,
+            backward_mode="time",
+            backward_time=5,
+        }})
+    end)
+
+    test("quick restart 2", function()
+        local ok, cfg, _ = update.validate_config({
+            one={kind="quick_restart", warmup_sec=5},
+            two={kind="quick_restart", warmup_sec=7},
+        })
+        assert(ok)
+        local stages = update.derive_pipeline(cfg)
+        assert.is.same(stages, {{
+            name="quick_restart",
+            processes={"one", "two"},
+            forward_mode="time",
+            forward_time=7,
+            backward_mode="time",
+            backward_time=7,
+        }})
     end)
 end)
