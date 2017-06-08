174ba9a002bf8f3ce727453b86cb60f31d62925d
Paul Colomiets <paul@colomiets.name>
2017-06-08 13:41:56 +0300

    updates: new algorithm of pipeline building based on topology sort

--- modules/update.lua
+++ modules/update.lua
@@ -1,5 +1,6 @@
 local package = (...):match("(.-)[^/]+$")
 local T = require(package..'trafaret')
+local func = require(package..'func')
 
 local SMOOTH_STAGES = 10
 
@@ -7,6 +8,7 @@ local CONFIG = T.Map { T.String {}, T.Or {
     T.Dict {
         run_before=T.List { T.String {} },
         [T.Key { "ack", default=true }]=T.Number {},
+        [T.Key { "duration", default=0 }]=T.Number {},
     },
     T.Dict {
         restart=T.Or {
@@ -18,7 +20,7 @@ local CONFIG = T.Map { T.String {}, T.Or {
         [T.Key { "warmup_sec", default=1 }]=T.Number {},
         [T.Key { "smooth_mode", optional=true }]=T.String {},
         [T.Key { "before", optional=true }]=T.List { T.String {} },
-        -- [T.Key { "after", optional=true }]=T.String {},
+        [T.Key { "after", optional=true }]=T.List { T.String {} },
     },
 }}
 
@@ -37,106 +39,179 @@ local function validate_config(config)
     return T.validate(CONFIG, config)
 end
 
-local function add_quick_restart(pipeline, daemon, cfg)
-    local insert_idx = 1
-    for idx, s in pairs(pipeline) do
-        if s.name == 'quick_restart' then
-            table.insert(s.processes, daemon)
-            -- predictable order, list is just few items, so is very quick
-            table.sort(s.processes)
-            if s.forward_time < cfg.warmup_sec then
-                s.forward_time = cfg.warmup_sec
-            end
-            if s.backward_time < cfg.warmup_sec then
-                s.backward_time = cfg.warmup_sec
-            end
-            return
-        elseif s.name == 'test_mode' then
-            insert_idx = idx+1
+local function add_quick_restart(stages, daemon, cfg)
+    local stage = stages['quick_restart']
+    if stage ~= nil then
+        table.insert(stage.processes, daemon)
+        -- predictable order, list is just few items, so is very quick
+        table.sort(stage.processes)
+        if stage.forward_time < cfg.warmup_sec then
+            stage.forward_time = cfg.warmup_sec
         end
+        if stage.backward_time < cfg.warmup_sec then
+            stage.backward_time = cfg.warmup_sec
+        end
+    else
+        stages['quick_restart'] = {
+            name="quick_restart",
+            forward_mode="time",
+            forward_time=cfg.warmup_sec,
+            backward_mode="time",
+            backward_time=cfg.warmup_sec,
+            processes={daemon},
+        }
     end
-    table.insert(pipeline, insert_idx, {
-        name="quick_restart",
-        forward_mode="time",
-        forward_time=cfg.warmup_sec,
-        backward_mode="time",
-        backward_time=cfg.warmup_sec,
-        processes={daemon},
-    })
 end
 
-local function add_test_mode(pipeline, daemon, cfg)
-    for _, s in pairs(pipeline) do
-        if s.name == 'test_mode' then
-            table.insert(s.processes, daemon)
-            -- predictable order, list is just few items, so is very quick
-            table.sort(s.processes)
-            if s.forward_time < cfg.warmup_sec then
-                s.forward_time = cfg.warmup_sec
-            end
-            if s.backward_time < cfg.warmup_sec then
-                s.backward_time = cfg.warmup_sec
-            end
-            return
+local function add_test_mode(stages, daemon, cfg)
+    local stage = stages['test_mode']
+    if stage ~= nil then
+        table.insert(stage.processes, daemon)
+        -- predictable order, list is just few items, so is very quick
+        table.sort(stage.processes)
+        if stage.forward_time < cfg.warmup_sec then
+            stage.forward_time = cfg.warmup_sec
         end
+        if stage.backward_time < cfg.warmup_sec then
+            stage.backward_time = cfg.warmup_sec
+        end
+    else
+        stages['test_mode'] = {
+            name="test_mode",
+            forward_mode="manual",
+            forward_time=cfg.warmup_sec,
+            backward_mode="time",
+            backward_time=cfg.warmup_sec,
+            processes={daemon},
+            before={"quick_restart", "smooth_restart"}, -- implicit before
+        }
     end
-    table.insert(pipeline, 1, {
-        name="test_mode",
-        forward_mode="manual",
-        forward_time=cfg.warmup_sec,
-        backward_mode="time",
-        backward_time=cfg.warmup_sec,
-        processes={daemon},
-    })
 end
 
-local function add_smooth_restart(pipeline, daemon, cfg)
-    local insert_idx = 1
-    for idx, s in pairs(pipeline) do
-        if s.name == 'smooth_restart' then
-            table.insert(s.processes, daemon)
-            -- predictable order, list is just few items, so is very quick
-            table.sort(s.processes)
-            if s.forward_time < cfg.warmup_sec*SMOOTH_STAGES then
-                s.forward_time = cfg.warmup_sec*SMOOTH_STAGES
+local function add_smooth_restart(stages, daemon, cfg)
+    local stage = stages['smooth_restart']
+    if stage ~= nil then
+        table.insert(stage.processes, daemon)
+        -- predictable order, list is just few items, so is very quick
+        table.sort(stage.processes)
+        if stage.forward_time < cfg.warmup_sec*SMOOTH_STAGES then
+            stage.forward_time = cfg.warmup_sec*SMOOTH_STAGES
+        end
+        if stage.backward_time < cfg.warmup_sec*SMOOTH_STAGES then
+            stage.backward_time = cfg.warmup_sec*SMOOTH_STAGES
+        end
+    else
+        stages['smooth_restart'] = {
+            name="smooth_restart",
+            forward_mode="smooth",
+            forward_time=cfg.warmup_sec*SMOOTH_STAGES,
+            backward_mode="smooth",
+            backward_time=cfg.warmup_sec*SMOOTH_STAGES,
+            processes={daemon},
+            after={"quick_restart"}, -- implicit after
+        }
+    end
+end
+
+local function add_command(_, _, _)
+end
+
+local function propagate_constraints(stages)
+    for _, stage in pairs(stages) do
+        local before = stage.before
+        if before ~= nil then
+            for _, cname in pairs(before) do
+                local cstage = stages[cname]
+                if cstage ~= nil then
+                    if cstage.after == nil then
+                        cstage.after = {}
+                    end
+                    -- duplicates are ok
+                    table.insert(cstage.after, stage.name)
+                end
             end
-            if s.backward_time < cfg.warmup_sec*SMOOTH_STAGES then
-                s.backward_time = cfg.warmup_sec*SMOOTH_STAGES
-            end
-            return
-        elseif s.name == 'test_mode' or s.name == 'quick_restart' then
-            if idx + 1 > insert_idx then
-                insert_idx = idx+1
+        end
+
+        local after = stage.after
+        if after ~= nil then
+            for _, cname in pairs(after) do
+                local cstage = stages[cname]
+                if cstage ~= nil then
+                    if cstage.before == nil then
+                        cstage.before = {}
+                    end
+                    -- duplicates are ok
+                    table.insert(cstage.before, stage.name)
+                end
             end
         end
     end
-    table.insert(pipeline, insert_idx, {
-        name="smooth_restart",
-        forward_mode="smooth",
-        forward_time=cfg.warmup_sec*SMOOTH_STAGES,
-        backward_mode="smooth",
-        backward_time=cfg.warmup_sec*SMOOTH_STAGES,
-        processes={daemon},
-    })
+end
+
+local function topology_sort(stages)
+    local result = {}
+    local visited = {}
+    local left = func.count_keys(stages)
+    while left > 0 do
+        local inserted_stages = 0
+        for sname, stage in pairs(stages) do
+            assert(stage.name == sname)
+            if not visited[sname] then
+                local can_be_inserted = true
+                for _, a in pairs(stage.after or {}) do
+                    if visited[a] == nil and stages[a] ~= nil then
+                        can_be_inserted = false
+                        break
+                    end
+                end
+                if can_be_inserted then
+                    table.insert(result, stage)
+                    visited[stage.name] = true
+                    left = left - 1
+                    inserted_stages = inserted_stages + 1
+                end
+            end
+        end
+        if inserted_stages == 0 then
+            print("Topology cycle between:",
+                table.concat(func.keys(stages), ", "))
+            return nil
+        end
+    end
+    return result
 end
 
 local function derive_pipeline(config)
-    local pipeline = {}
+    local stages = {}
     for daemon, cfg in pairs(config) do
         if cfg.restart == "quick" then
-            add_quick_restart(pipeline, daemon, cfg)
+            add_quick_restart(stages, daemon, cfg)
             if cfg.test_mode_percent > 0 then
-                add_test_mode(pipeline, daemon, cfg)
+                add_test_mode(stages, daemon, cfg)
             end
         elseif cfg.restart == "smooth" then
-            add_smooth_restart(pipeline, daemon, cfg)
+            add_smooth_restart(stages, daemon, cfg)
             if cfg.test_mode_percent > 0 then
-                add_test_mode(pipeline, daemon, cfg)
+                add_test_mode(stages, daemon, cfg)
             end
-        -- else
-            -- TODO(tailhook) this is migration/onetime commands
+        elseif cfg.run_before ~= nil then
+            add_command(cfg)
+        else
+            print("Error, can't classify daemon", daemon)
         end
     end
+    propagate_constraints(stages)
+    local pipeline = topology_sort(stages)
+    if pipeline == nil then
+        return nil
+    end
+
+    -- cleanup already applied constraints
+    for _, item in pairs(pipeline) do
+        item.before = nil
+        item.after = nil
+    end
+
     local ok, result, err = T.validate(PIPELINE, pipeline)
     if not ok then
         for _, e in pairs(err) do
